d01bb05b9eb78fc2e019981f3fc87ae63f147f38
Britt Ballard <davidbritton.ballard@gmail.com>
2012-01-18 09:19:16 -0600

    Created a VERY simple eample of what selenium webdriver can do.

---
+++ SeleniumWebDriverExample/SeleniumWebDriver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumWebDriverExample
+{
+    [TestClass]
+    public class SeleniumWebDriver
+    {
+        [TestMethod]
+        public void EampleOne()
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://order.papajohns.com/storelocator/page.html");
+            IWebElement streetAddress = driver.FindElement(By.Id("streetAddress"));
+            streetAddress.SendKeys("3800 Commerce");
+            IWebElement zip = driver.FindElement(By.Id("zip"));
+            zip.SendKeys("75226");
+            IWebElement searchButton = driver.FindElement(By.Id("mainSearchBtn"));
+            searchButton.Click();
+            var title = driver.Title;
+
+            // driver.Quit();
+        }
+    }
+}
